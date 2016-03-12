96114e48bc2c75cbdcabb78cd3f6a02842b41a67
DmytroChernenko <dmytro.chernenko@ex.ua>
2016-03-12 16:17:04 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+/**
+ * Created by Dmytro on 12.03.16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
