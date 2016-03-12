b16ede1bde91985f710faf964de965fc28945f66
DmytroChernenko <dmytro.chernenko@ex.ua>
2016-03-12 16:57:57 +0200

    Add empty classes

---
+++ src/main/java/com/chernenko/carparser/dao/HtmlCarDao.java
@@ -0,0 +1,7 @@
+package com.chernenko.carparser.dao;
+
+/**
+ * Created by Dmytro on 12.03.16.
+ */
+public class HtmlCarDao {
+}
---
+++ src/main/java/com/chernenko/carparser/entity/Car.java
@@ -0,0 +1,7 @@
+package com.chernenko.carparser.entity;
+
+/**
+ * Created by Dmytro on 12.03.16.
+ */
+public class Car {
+}
